0ad0083d1228a761f547e4e4ec6e2a3117cda82e
root <root@localhost.localdomain>
2020-08-16 16:26:19 +0000

    form

---
+++ form.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+<h1>The form element</h1>
+
+<form action="/action_page.php">
+  <label for="fname">First name:</label>
+  <input type="text" id="fname" name="fname"><br><br>
+  <label for="lname">Last name:</label>
+  <input type="text" id="lname" name="lname"><br><br>
+  <input type="submit" value="Submit">
+</form>
+</body>
+</html>
