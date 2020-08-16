edae865fc1a369e1582fa61a2d81567e72f4e7aa
Parrot Live user <user@localhost.localdomain>
2020-08-16 15:30:13 +0000

    php adding

---
+++ serverconfig.php
@@ -0,0 +1,10 @@
+<?php
+echo "This is the server config\n";
+echo $_SERVER['SERVER_NAME'];
+echo "<br>";
+echo $_SERVER['SERVER_ADDR'];
+echo "<br>";
+echo $_SERVER['REQUEST_TIME'];
+echo "<br>";
+echo $_SERVER['HTTP_ACCEPT'];
+?>
