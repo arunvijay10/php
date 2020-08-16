72bbd006150930c8e966cb41b33b22e1a2d2c842
Parrot Live user <user@localhost.localdomain>
2020-08-16 14:00:45 +0000

    php adding

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+echo "hello world \n";
+echo strrev("This is to be repeated \n");
+name();
+add();
+$name="arun vijay";
+var_dump($name);
+
+function add()
+{
+    $first = 24;
+    $second = 25;
+    $third = $first + $second;
+    echo "$third";
+}
+function name() {
+echo "this is a function\n";
+};
+?>
