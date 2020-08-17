500d3996be90ea26bedaeeba1d729a37a9fbd06a
Parrot Live user <user@localhost.localdomain>
2020-08-17 17:11:08 +0000

    adding

--- var_dump.php
+++ var_dump.php
@@ -2,4 +2,8 @@
 $ar =array("this is  going to give a detail info");
 array_push($ar,45);
 var_dump($ar);
+$arr =array("This is a next level","we have to delete the file");
+var_dump($arr);
+unset($arr[1]);
+var_dump($arr);
 ?>
