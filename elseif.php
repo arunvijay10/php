afa93253dc6686bd8563042d551fa88c81771117
Parrot Live user <user@localhost.localdomain>
2020-08-17 17:29:06 +0000

    adding

---
+++ elseif.php
@@ -0,0 +1,17 @@
+<?php
+$name="Arun Vijay";
+$name="arun vijay";
+if($name =="Arun Vijay")
+{
+    echo "He is a awesome Programmer";
+}
+elseif($name == "arun vijay")
+{
+    echo "He is a super coder";
+}
+else
+{
+    echo "He Is Building A app That's why he is busy";
+}
+
+?>
