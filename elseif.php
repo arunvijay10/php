ad6ffb83c757d4d1b845ecaa45a5bb0efacecaaf
Parrot Live user <user@localhost.localdomain>
2020-08-17 17:31:54 +0000

    adding

--- elseif.php
+++ elseif.php
@@ -3,15 +3,16 @@ $name="Arun Vijay";
 $name="arun vijay";
 if($name =="Arun Vijay")
 {
-    echo "He is a awesome Programmer";
+    echo "He is a awesome Programmer\n";
 }
 elseif($name == "arun vijay")
 {
-    echo "He is a super coder";
+    echo "He is a super coder\n";
 }
 else
 {
     echo "He Is Building A app That's why he is busy";
 }
-
+var_dump($name);
+print_r($name);
 ?>
