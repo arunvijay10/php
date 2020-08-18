79cb9409a509dc6913dfdf8f2552285e2351068e
root <root@localhost.localdomain>
2020-08-18 12:16:56 +0000

    object

---
+++ object.php
@@ -0,0 +1,18 @@
+<?php
+class student
+{
+public $name;
+private $age;
+function addname($n)
+{
+	$this->name=$n;
+}
+function getname($n)
+{
+	return $this->name;
+}
+$obj=new student();
+$obj.name("arun Vijay");
+obj.getname();
+}
+?>
