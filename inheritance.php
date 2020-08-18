9421492c614cae78624355c6e188adf5d8ea0923
root <root@localhost.localdomain>
2020-08-18 13:23:57 +0000

    inheritance

---
+++ inheritance.php
@@ -0,0 +1,32 @@
+<?php
+
+//creating a constructor
+
+
+
+class  students{
+	protected $class;
+	protected $fee;
+	protected $books;
+
+function __construct()
+{
+        echo "name this as a contstructor";
+}
+
+}
+
+class student1 extends students
+{
+	public $anyone;
+
+	function __construct()
+	{
+	parent::__construct();
+	echo "this is a student one";
+}
+}
+
+$objstudent1 = new student1();
+?>
+
